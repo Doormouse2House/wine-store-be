f855ed2531168667d70da61c0bf25a401b1e2ab5
Graham <grahamrmoss@gmail.com>
2017-12-17 20:46:52 +0000

    WP-29 first set of model attributes

--- api/models/Producer.js
+++ api/models/Producer.js
@@ -9,6 +9,55 @@ module.exports = {
 
   attributes: {
 
+    // Producer name
+    name: {
+      type: 'string',
+      unique: true,
+      required: true
+    },
+
+    address: {
+      type: 'string'
+    },
+
+    web_address: {
+      type: 'url',
+      unique: true
+    },
+
+    region: {
+      type: 'string',
+      required: true
+    },
+
+    appellation: {
+      type: 'string'
+    },
+
+    owner: {
+      type: 'string'
+    },
+
+    enologist: {
+      type: 'string'
+    },
+
+    agricultural_lead: {
+      type: 'string'
+    },
+
+    planted_hectares: {
+      type: 'number'
+    },
+
+    total_production: {
+      type: 'number'
+    },
+
+    description: {
+      type: 'string'
+    }
+
   }
 };
 
